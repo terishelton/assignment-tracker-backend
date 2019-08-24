f1c818d479e55e0e56a1359772df957391838f05
Teri Shelton <39071165+terishelton@users.noreply.github.com>
2019-08-24 16:35:45 -0700

    minor tweaks

--- api/models/user.js
+++ api/models/user.js
@@ -3,7 +3,6 @@ const Schema = mongoose.Schema
 const Assignment = require('./assignment')
 
 const personSchema = Schema({
-    _id: Schema.Types.ObjectId,
     email: {
         type: String,
         required: true,
